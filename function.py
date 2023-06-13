119e5dedfc04bab88cfaf6457e3363646a2f0078
joshking1 <88409463+joshking1@users.noreply.github.com>
2023-06-13 12:45:03 -0400

    Create function.py

---
+++ function.py
@@ -0,0 +1,10 @@
+def print_cousin_info():
+    first_name = input("Enter your cousin's first name: ")
+    last_name = input("Enter your cousin's last name: ")
+    age = input("Enter your cousin's age: ")
+    grade = input("Enter your cousin's current grade in school: ")
+    
+    print("My first name is {}, my last name is {}. I am in grade {}, and I love mathematics.".format(first_name, last_name, grade))
+
+# Example usage:
+print_cousin_info()
